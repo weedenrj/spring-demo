576f918623ecae2b9ef7734fdc6fa5a61709967f
Rob Weeden <rob@4-cube.io>
2024-07-17 13:26:11 -0500

    built greeting api

---
+++ src/main/java/com/api/model/Greeting.java
@@ -0,0 +1,3 @@
+package com.api.model;
+
+public record Greeting(long id, String context) {}
--- src/main/java/com/frontend/DemoApplication.java
+++ src/main/java/com/frontend/DemoApplication.java
@@ -1,4 +1,5 @@
-package com.example.demo;
+package com.frontend;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,7 @@ public class DemoApplication {
       SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
