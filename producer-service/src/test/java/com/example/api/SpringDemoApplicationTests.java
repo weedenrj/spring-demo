83585858b848b53135f7b6111bb9cab23c4fb9b6
Rob Weeden <rob@4-cube.io>
2024-07-17 17:58:12 -0500

    add type anotation to tests

--- producer-service/src/test/java/com/example/api/SpringDemoApplicationTests.java
+++ producer-service/src/test/java/com/example/api/SpringDemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringDemoApplicationTests.class)
 class SpringDemoApplicationTests {
 
 	@Test
